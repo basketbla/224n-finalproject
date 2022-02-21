0b8094357eb51fecb0c0539eb2c32e8a3f7dec97
Christine Manegan <christinemanegan@DN51utci.SUNet>
2022-02-21 13:45:37 -0800

    added all labeled finetuning data

--- clean_data.py
+++ clean_data.py
@@ -8,9 +8,14 @@ def findURLS(string):
   
     # findall() has been used 
     # with valid conditions for urls in string
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex,string)      
-    return [x[0] for x in url]
+    words = []
+    for word in string: 
+    	if "http" in word or "www" in word: 
+    		words.append(word)
+    #regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    #url = re.findall(regex,string)      
+    #return [x[0] for x in url]
+    return words
 
 
 def extract_emojis(s):
@@ -18,30 +23,46 @@ def extract_emojis(s):
 
 labels = []
 data = []
-directory = 'ed_subreddit_data_negative'
+directories = ['ed_subreddit_data_positive', 'ed_subreddit_data_negative']
 
-for filename in os.listdir(directory):
-	f = os.path.join(directory, filename)
-	if os.path.isfile(f):
-		csv = pd.read_csv(f)
-		bodies = csv.body.tolist()
-		i = 0
-		for body in bodies:
-			i += 1
-			print(i)
-			body = str(body)
-			body.replace("\n", "")
-			body.replace("&#x200B;", "")
-			urls = findURLS(body)
-			for url in urls: 
-				body.replace(url, "")
-			emojis = extract_emojis(body)
-			for e in emojis: 
-				body.replace(e, "")
-			data.append(body)
-			labels.append(0)
+
+file = 0
+for directory in directories: 
+	for filename in os.listdir(directory):
+		print("file: ", filename)
+		file += 1
+		f = os.path.join(directory, filename)
+		if os.path.isfile(f):
+			try: 
+				csv = pd.read_csv(f)
+			except:
+				continue
+			bodies = csv.body.tolist()
+			post = 0
+			print("total num posts: ",len(bodies))
+			for body in bodies:
+				print("postno: ", post)
+				print(body)
+				post += 1
+				body = str(body)
+				body.replace("\n", "")
+				body.replace("&#x200B;", "")
+				urls = findURLS(body)
+				for url in urls: 
+					body.replace(url, "")
+				emojis = extract_emojis(body)
+				for e in emojis: 
+					body.replace(e, "")
+				data.append(body)
+				if directory == 'ed_subreddit_data_positive':
+					labels.append(1)
+				else: 
+					labels.append(0)
+print("ALL DONE")
 df = pd.DataFrame({"labels": labels, "post": data})
-df.to_csv("negative_labeled_data")
+print(df)
+df.to_csv("total_labeled_data")
+
 
 
 		
